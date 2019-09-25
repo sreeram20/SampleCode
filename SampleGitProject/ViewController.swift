0deaa7f49d1e6e6f721545bcfa3d6ea93b0e0b29
sreeram <sreeram.r@byjus.com>
2019-09-25 21:03:50 +0530

    Initial Commit

---
+++ SampleGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleGitProject
+//
+//  Created by Sreeram R on 25/09/19.
+//  Copyright © 2019 Sreeram R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SampleGitProjectTests/SampleGitProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleGitProjectTests.swift
+//  SampleGitProjectTests
+//
+//  Created by Sreeram R on 25/09/19.
+//  Copyright © 2019 Sreeram R. All rights reserved.
+//
+
+import XCTest
+@testable import SampleGitProject
+
+class SampleGitProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
